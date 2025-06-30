8d3443825760b83a897f949dd3bc1a2cbf018f8f
Maxime Jeannin <maxime.jeannin@hotmail.fr>
2025-06-30 11:48:31 +0200

    feat: initialisation du projet SpringBoot

---
+++ src/main/java/fr/eni/encheres/IciOnVendApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IciOnVendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IciOnVendApplication.class, args);
+    }
+
+}
