d4e24af08abcd3a5ce5a898153f7a1151e5158ef
Maxime Jeannin <maxime.jeannin@hotmail.fr>
2025-07-01 15:54:01 +0200

    fragment newUser

--- src/main/java/fr/eni/encheres/controller/LoginController.java
+++ src/main/java/fr/eni/encheres/controller/LoginController.java
@@ -76,12 +76,6 @@ public class LoginController {
         return "redirect:/index";
     }
 
-
-    @ModelAttribute("connectedUser")
-    public User AddUser(){
-        System.out.println("Add Attribut User to Session");
-        return new User();
-
     @GetMapping("/logout")
     public String finSession(SessionStatus status) {
         // Suppression des attributs de @SessionAttributs
