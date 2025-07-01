2e1846287402de9c8654647128c60d815c35cbd7
Maxime Jeannin <maxime.jeannin@hotmail.fr>
2025-07-01 12:44:33 +0200

    SQL: scripts modifiés, ajout d'un bidId. Ajout de fonctions dans BidDAO et BidDAOImpl

--- src/main/java/fr/eni/encheres/dal/BidDAO.java
+++ src/main/java/fr/eni/encheres/dal/BidDAO.java
@@ -1,6 +1,13 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.bo.Bid;
+
+import java.util.List;
+
 public interface BidDAO {
-    
+    int createBid();
+    Bid read(long id);
+    List<Bid> readAllFromArticleId(long articleId);
+    List<Bid> readAllFromArticleIdByUserId(long articleId,long userId);
 
 }
